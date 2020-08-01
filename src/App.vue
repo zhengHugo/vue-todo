e0c4534011e55f9ad5ac39bfb8e250f8c47a697f
Hugo Zheng <zhenghugo515@gmial.com>
2020-08-01 00:08:07 -0400

    finish basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,50 @@
 <template>
   <div id="app">
+    <Header/>
+    <AddTodo/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
+import Todos from "@/components/Todos";
+import Header from "@/components/layout/Header";
+import AddTodo from "@/components/AddTodo";
 
 export default {
-  name: 'App',
-  components: {}
-}
+  name: "App",
+  components: {
+    Todos,
+    Header,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false,
+        },
+      ],
+    };
+  },
+  methods:{
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
+  }
+};
 </script>
 
 <style>
@@ -21,5 +57,18 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
